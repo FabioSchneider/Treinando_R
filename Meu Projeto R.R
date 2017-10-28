e096ca4110bd0d86ea202b99cabb0a097204e41f
FabioSchneider <33033563+FabioSchneider@users.noreply.github.com>
2017-10-28 14:22:13 -0200

    Update Meu Projeto R.R

--- Meu Projeto R.R
+++ Meu Projeto R.R
@@ -1,37 +1,37 @@
 # Curso Data Scientist
-# Módulo 2 - Linuagem R
+# MÃ³dulo 2 - Linuagem R
 
-# Criar um dataset com pelo menos cinco colunas numéricas e três categóricas.
-# Use as funções de criação de distribuição aleatória, use pelo menos duas
+# Criar um dataset com pelo menos cinco colunas numÃ©ricas e trÃªs categÃ³ricas.
+# Use as funÃ§Ãµes de criaÃ§Ã£o de distribuiÃ§Ã£o aleatÃ³ria, use pelo menos duas
 # Grave seu dataset no disco
-# Você deverá extrair do dataset:
+# VocÃª deverÃ¡ extrair do dataset:
 
 #EXTRAIR DO DATASET
-# Somas e medias das colunas numéricas do dataset usando ??pply
+# Somas e medias das colunas numÃ©ricas do dataset usando ??pply
 # Usar split
-# Mostra um gráfico
+# Mostra um grÃ¡fico
 # Histograma
 
-#Projeto para controle de Diabéticos
+#Projeto para controle de DiabÃ©ticos
 #Glicose normal em Jejum de 70 a 99 mg/dl
 
-#Total de Beneficiários Ativos na Cooperativa
+#Total de BeneficiÃ¡rios Ativos na Cooperativa
 B <- 300000
-#10% dos Beneficiários Ativos
+#10% dos BeneficiÃ¡rios Ativos
 b <- 30000
 
 
 # Criar um dataset com pelo menos cinco colunas
 Identificador_Beneficiario <- 1:3000
-peso <- abs(round(rnorm(b,75,20)))
-Altura <- abs(rnorm(b,1.7,0.2))
+peso <- round(rnorm(b,75,20),digits=3)
+Altura <- round(rnorm(b,1.7,0.2),digits=2)
 Sexo <- round(runif(b,min = 0, max = 1),0)
 glicose <- rnorm(b,87,20)
 
-# Criar um dataset com pelo menos três categóricas.
+# Criar um dataset com pelo menos trÃªs categÃ³ricas.
 cat.refri <- rep(c(0, 1, 2, 3, 4, 5, 6), B) #rep -> repete os numeros de 1 a 6 
-set.seed(1234) #Seto um número fixo para minha semente
-refri.temp <- sample(cat.refri, b) # Busco uma amostra de 3000 beneficiários
+set.seed(1234) #Seto um nÃºmero fixo para minha semente
+refri.temp <- sample(cat.refri, b) # Busco uma amostra de 3000 beneficiÃ¡rios
 refri <- factor(refri.temp, levels = c(0,1,2,3,4,5,6), labels = c("Nunca","1x Semana", "2x Semana", "3x Semana", "4x Semana","5x Semana", "6 ou Mais"), ordered = TRUE) #Crio as categorias e seus label's
 rm(cat.refri, refri.temp)
 str(refri)
@@ -39,14 +39,14 @@ summary(refri)
 
 set.seed(1234)
 cat.PraticaEsporte.n <- rbinom(b, 1, 0.8)
-praticaEsporte <- factor(cat.PraticaEsporte.n, levels = c(0, 1), labels = c("sim", "não"), ordered = TRUE)
+praticaEsporte <- factor(cat.PraticaEsporte.n, levels = c(0, 1), labels = c("sim", "nÃ£o"), ordered = TRUE)
 str(praticaEsporte)
 summary(praticaEsporte)
 
 set.seed(1234)
 catHistoricoFamiliar.n <- rbinom(b, 1, 0.9)
-historicoFamiliar <- factor(catHistoricoFamiliar.n, levels = c(0, 1), labels = c("sim", "não"), ordered = TRUE)
-str(historicoFamiliar) #Exibe o conteúdo de forma compactada
+historicoFamiliar <- factor(catHistoricoFamiliar.n, levels = c(0, 1), labels = c("sim", "nÃ£o"), ordered = TRUE)
+str(historicoFamiliar) #Exibe o conteÃºdo de forma compactada
 summary(historicoFamiliar) #Sumariza o resultado
 
 
@@ -65,38 +65,37 @@ str(DiabeticosDF)
 # Grave seu dataset no disco
 write.table(DiabeticosDF, file = "ProjetoPacientesDiabeticos.csv", sep = ",", col.names = TRUE, fileEncoding = "UTF-8")
 
-# Sumarizando os dados: Exibindo (Valor Mínimo, 1º Quartil, Meidana, Média, 3º Quartil e Valor Máximo)
+# Sumarizando os dados: Exibindo (Valor MÃ­nimo, 1Âº Quartil, Meidana, MÃ©dia, 3Âº Quartil e Valor MÃ¡ximo)
 sapply(DiabeticosDF[,c("Identificador_Beneficiario", "peso", "Altura", "Sexo", "glicose", "refri", "praticaEsporte","historicoFamiliar")],summary)
 
-# Exibindo a Soma das colunas numéricas 
+# Exibindo a Soma das colunas numÃ©ricas 
 apply(DiabeticosDF[,c("Identificador_Beneficiario", "peso", "Altura", "Sexo", "glicose")],2,sum)
 
-# Mostando gráfico histograma da Glicose
+# Mostando grÃ¡fico histograma da Glicose
 hist(DiabeticosDF$glicose,
      main = "Histograma da Glicose",
      xlab = "Glicose (em mg/dl)",
-     ylab = "Freqüência",
+     ylab = "FreqÃ¼Ãªncia",
      border="black", 
      col="pink")
 
-# Mostrando Gráfico de Pontos para peso x glicose, usando a cor para identificar frequência de consumo de Refrigerante
+# Mostrando GrÃ¡fico de Pontos para peso x glicose, usando a cor para identificar frequÃªncia de consumo de Refrigerante
 ggplot(data=DiabeticosDF, aes(x=peso, y=glicose, color = refri)) + geom_point()
 
-# Mostrando Gráfico de Área para glicose x altura
+# Mostrando GrÃ¡fico de Ãrea para glicose x altura
 ggplot(data=DiabeticosDF, aes(x=glicose, y=Altura)) + geom_area()
 
 # Usando Split
 
 #Pegando as 6 primeiras linhas
 head(DiabeticosDF)
-# Atribuindo as 6 primeiras linhas à A
+# Atribuindo as 6 primeiras linhas Ã  A
 a <- head(DiabeticosDF)
 # Fazendo o Split de A por Sexo
 split(a, a$Sexo)
 # Fazendo split por praticantes de esporte
 split(a, a$praticaEsporte)
 
-#Espero que tenha entendido e atendido todos os pontos da solicitação
-#Aguardo críticas para melhoria na forma de programar, assim como na idealização dos projetos
-#Fábio Junior Schneider Marcelino
-
+#Espero que tenha entendido e atendido todos os pontos da solicitaÃ§Ã£o
+#Aguardo crÃ­ticas para melhoria na forma de programar, assim como na idealizaÃ§Ã£o dos projetos
+#FÃ¡bio Junior Schneider Marcelino
